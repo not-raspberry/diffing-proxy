cf74df112ef418f605f0d2374ba9aedc8c35e5d0
Michał Pawłowski <unittestablecode@gmail.com>
2016-05-15 17:15:53 +0200

    Make it almost work like a Very Bad cache

--- project.clj
+++ project.clj
@@ -5,11 +5,13 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.json "0.2.6"]
                  [org.clojure/tools.cli "0.3.4"]
+                 [org.clojure/tools.logging "0.3.1"]
                  [compojure "1.5.0"]
                  [ring "1.4.0"]
                  [clj-http "2.2.0"]
                  [differ "0.3.1"]
-                 [slingshot "0.12.2"]]
+                 [slingshot "0.12.2"]
+                 [cheshire "5.6.1"]]
   :main ^:skip-aot diffing-proxy.main
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
--- src/diffing_proxy/diffing.clj
+++ src/diffing_proxy/diffing.clj
@@ -1,18 +1,47 @@
 (ns diffing-proxy.diffing
   (:require [clj-http.client :as client]
+            [clojure.tools.logging :as log]
             [clojure.data.json :as json]
             [differ.core :refer [diff patch]]
-            [slingshot.slingshot :refer [try+]]))
+            [cheshire.core :refer [parse-string]]
+            [slingshot.slingshot :refer [try+]])
+  (:import [com.fasterxml.jackson.core JsonParseException]))
 
+; Schema behind the atom:
+; {"/path1"
+;  {1 {"version" 1, "some_data" [], "sth_else" {}}
+;   2 {"version" 2, "some_data" [1 2 3], "sth_else" {"a" 12}}
+;   4 {"version" 4, "some_data" [1 2 3 4], "sth_else" {"a" 14}}}
+;  "/path2" {}}
 (def cached-versions (atom {}))
+
+(defn update-cache [cache path version body]
+  (assoc-in cache [path version] body))
+
+(defn cache-response! [path version body]
+  (swap! cached-versions #(update-cache % path version body)))
+
+(defn integrate-response!
+  [path parsed-response-body]
+  (let [version (parsed-response-body "version")]
+    (if (number? version)
+      (cache-response! path version parsed-response-body)
+      (log/error "Unversioned reponse to" path))))
 
 (defn handle-diffed-route [base-backend-address request]
   (try+
-    (let [resource-url (str base-backend-address (:uri request))
-          backend-response (client/get resource-url)]
-        (:body backend-response))
+    (let [path (:uri request)
+          resource-url (str base-backend-address path)
+          body (:body (client/get resource-url))
+          parsed-json (parse-string body)]
+      (integrate-response! path parsed-json)
+      body)
     (catch [:status 404] _
-        {:status 404, :body "Not Found on the backend"})
+      {:status 404, :body "Not Found on the backend"})
+    (catch JsonParseException e
+      (log/error "Backend responded an invalid JSON:" (.getMessage e))
+      {:status 502
+       :body (str "Bad Gateway\nBackend responded with an invalid JSON.")})
     (catch map? {:keys [:status]}
-        {:status 502
-         :body (str "Bad Gateway\nBackend responded with " status)})))
+      {:status 502
+       :body (str "Bad Gateway\nBackend responded with " status)})))
